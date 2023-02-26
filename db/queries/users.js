38e5ca689b3484d678b4f0fa767bce2732db50f0
Sam Maisha <sam.maisha99@gmail.com>
2023-02-26 05:07:03 +0000

    create file for user related quiries.add empty functions as placeholders for queries which will be added later

---
+++ db/queries/users.js
@@ -0,0 +1,11 @@
+const db = require('../connection');
+
+// get quiz results for user from quiz_results table (QUIZ RESULT)
+getQuizResultsForUser = function() {
+}
+
+// get quizzes that were created by a user (MY QUIZZES)
+getQuizzesCreatedByUser = function() {
+
+}
+
