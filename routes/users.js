2a97a69ad623cc88e98e4cbe15492b5cc2336e10
Sam Maisha <sam.maisha99@gmail.com>
2023-02-26 17:05:21 +0000

    update GET routes for /:userid/results, /:userid/quizzes and /:userid/quizzes/:quizid to render pages for each

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,23 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('users');
-}); 
+// QUIZ RESULTS - render page for list of quizzes and results user has taken
+router.get('/:userid/results', (req, res) => {
+  res.render('user_results');
+});
+
+// MY QUIZZES - render page to show list of quizzes user has created
+router.get('/:userid/quizzes', (req, res) => {
+  res.render('user_quizzes');
+})
+
+
+// SHOW MY QUIZ RESULT - render page for indiviudal quiz result user has taken
+router.get('/:userid/quizzes/:quizid', (req, res) => {
+  res.render('user_quiz_result');
+})
+
+
+
 
 module.exports = router;
