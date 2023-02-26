8bc5b2ebf467b1baa78394c046002aa51cc970e0
Sam Maisha <sam.maisha99@gmail.com>
2023-02-26 22:58:22 +0000

    merge feature/routes branch and resolve conflicts

--- routes/users.js
+++ routes/users.js
@@ -31,4 +31,29 @@ router.get('/:userid/quizzes/:quizid', (req, res) => {
   res.render('user_quiz_result');
 })
 
+// QUIZ RESULTS - render page for list of quizzes and results user has taken
+router.get('/:userid/results', (req, res) => {
+  // query returns a list of quizzes, results and result links as an array of objects
+  // pass results into templateVars
+  userQueries.getQuizResultsForUser()
+  .then()
+  .catch()
+
+  res.render('user_results');
+});
+
+// MY QUIZZES - render page to show list of quizzes user has created
+router.get('/:userid/quizzes', (req, res) => {
+  res.render('user_quizzes');
+})
+
+
+// SHOW MY QUIZ RESULT - render page for indiviudal quiz result user has taken
+router.get('/:userid/quizzes/:quizid', (req, res) => {
+  res.render('user_quiz_result');
+})
+
+
+
+
 module.exports = router;
