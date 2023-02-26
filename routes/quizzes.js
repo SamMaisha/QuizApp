99a37087785239a64bebb9456fd927a03ef442a4
Sam Maisha <sam.maisha99@gmail.com>
2023-02-26 22:13:34 +0000

    merge feature/routes and resolbve conflicts in code

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -11,31 +11,50 @@ const quizQueries = require('../db/queries/quizzes');
 
 // HOMEPAGE - show list of public quizzes
 router.get('/', (req, res) => {
-  quizQueries.getQuizzes()
-  // returns an array of objects
-    .then(users => {
-      res.json({ users });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
+  // query returns an array of objects
+  const templateVars = {
+    // just pass the query result
+  }
+  res.render('index', templateVars);
+
+  // quizQueries.getQuizzes()
+  //   .then(users => {
+  //     res.json({ users });
+  //   })
+  //   .catch(err => {
+  //     res
+  //       .status(500)
+  //       .json({ error: err.message });
+  //   });
 });
 
-// CREATE QUIZ - render page
+// CREATE QUIZ - render page to create a new quiz
 router.get('/create', (req, res) => {
-
+  res.render('quiz_create');
 })
 
 // CREATE QUIZ - add newly created quiz to DB table
 router.post('/',(req,res) => {
+  //parse body for submitted quiz
+  const newQuiz = req.body;
+  console.log(newQuiz);
+  quizQueries.addNewQuiz(newQuiz)
+  .then(quiz => {
+    if (!quiz) {
+      res.send("oops something went wrong! Please try submitting again")
+      return;
+    }
+    res.send("new quiz has been created!")
+  })
+  .catch(error => res.send(error));
 
 })
 
 
 // VIEW QUIZ - show single quiz for user to attempt
 router.get('/:quizid', (req, res) => {
+  // query should return an object
+  quizQueries.getSelectedQuiz(quizId)
 
 })
 
@@ -44,5 +63,9 @@ router.post('/:quizid', (req, res) => {
 
 })
 
+// MY QUIZZES - post route to delete a quiz {POST MVP}
+router.post('/:quizid/delete', (req, res) => {
+
+})
 
 module.exports = router;
