3ecf1b7d9b2ce2bbf1ca18b4874f2fa593d1e517
Sam Maisha <sam.maisha99@gmail.com>
2023-02-27 23:23:36 +0000

    refactored getQuizResultsForUser and exported functions

--- db/queries/users.js
+++ db/queries/users.js
@@ -2,8 +2,8 @@ const { param } = require('express/lib/request');
 const db = require('../connection');
 
 // get quiz results for user from quiz_results table (QUIZ RESULT)
-getQuizResultsForUser = function(user) {
-    const queryParams = [user.id];
+const getQuizResultsForUser = function(userId) {
+    const queryParams = [userId];
     const parameterizedQuery = 'SELECT quiz_results.*, quizzes.title FROM quiz_results JOIN quizzes ON quiz_id = quizzes.id WHERE user_id = $1'
     return db.query(parameterizedQuery,queryParams)
       .then(data => {
@@ -14,7 +14,7 @@ getQuizResultsForUser = function(user) {
 };
 
 // get quizzes that were created by a user (MY QUIZZES)
-getQuizzesCreatedByUser = function(user) {
+const getQuizzesCreatedByUser = function(user) {
     const queryParams = [user.id];
     const parameterizedQuery = 'SELECT * FROM quizzes WHERE owner_id = $1'
     return db.query(parameterizedQuery,queryParams)
@@ -25,3 +25,4 @@ getQuizzesCreatedByUser = function(user) {
       });
 }
 
+module.exports = { getQuizResultsForUser, getQuizzesCreatedByUser}
