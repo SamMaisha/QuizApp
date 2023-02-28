2e631bcbcc5c31de5abea8c103daba50e3f3e212
Sam Maisha <sam.maisha99@gmail.com>
2023-02-28 12:07:39 +0000

    add folder for helper functions

---
+++ functions/helper.js
@@ -0,0 +1,26 @@
+const pushAnswersIntoQuestionObject = (array1, array2) => {
+  const updatedQuestionArray = array1.map(questionObject => {
+    const answersArrayForEachQuestion = array2.filter(answersObject => {
+      return questionObject.question_id === answersObject.question_id
+    })
+
+    questionObject["answers"] = answersArrayForEachQuestion;
+    console.log("RESULT TEST" ,questionObject);
+    return questionObject;
+  })
+  return updatedQuestionArray;
+}
+
+const pushAnswerIntoQuestion = (array1, array2) => {
+  const updatedArray = array1.map(x => {
+    const questionAnswers = array2.filter(y => {
+      return x.question_id === y.question_id
+    })
+
+    x["answers"] = questionAnswers;
+    console.log("RESULT TEST" ,x);
+    return x;
+  })
+  return updatedArray;
+}
+module.exports = { pushAnswersIntoQuestionObject }
