f23a65498ec6f556050b50125f0fe15474363937
Daniel <danielseniv@outlook.com>
2023-03-03 05:53:01 -0500

    Add query to return all quizzes attempted by user

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,11 +1,15 @@
+const { param } = require('express/lib/request');
 const db = require('../connection');
 
 // get quiz results for user from quiz_results table (QUIZ RESULT)
-getQuizResultsForUser = function() {
+getQuizResultsForUser = function(user) {
+    const queryParams = [user.id];
+    const parameterizedQuery = 'SELECT * FROM quiz_results WHERE user_id = $1'
+    return db.query(parameterizedQuery,queryParams);
 }
 
 // get quizzes that were created by a user (MY QUIZZES)
-getQuizzesCreatedByUser = function() {
-
+getQuizzesCreatedByUser = function(user) {
+    
 }
 
