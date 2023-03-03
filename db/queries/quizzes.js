31ba42dfc536d15cae5e5326776e6b7e12ec7847
Daniel <danielseniv@outlook.com>
2023-03-03 05:36:07 -0500

    Add query to insert user quiz result into database

--- db/queries/quizzes.js
+++ db/queries/quizzes.js
@@ -1,3 +1,4 @@
+const { compile } = require('ejs');
 const db = require('../connection');
 
 // get all public quizzes from quizzes table (HOMEPAGE)
@@ -14,18 +15,20 @@ const getSelectedQuiz = function(quiz_id) {
   return db.query('SELECT quizzes.id, title, description, quiz_questions.* FROM quiz_questions JOIN quizzes ON quiz_id = quizzes.id')
   .then(data => {
     const quizData = data.rows;
-    return quizData;
+    return quizData; //will make this function parameterized
   })
 }
 
 // insert results to quiz_results table after user has taken quiz (VIEW QUIZ)
-const addQuizResult = function() {
+
+const addQuizResult = function(quizResultData) {
+  const queryParams = [quizResultData.id, quizResultData.quiz_id, quizResultData.user_id, quizResultData.score, quizResultData.completed_at, quizResultData.link];
+  const parameterizedQuery = 'INSERT INTO quiz_results VALUES ($1, $2, $3, $4, $5, $6)';
+  return db.query(parameterizedQuery, queryParams);
 }
 
 // insert new quiz to relevant tables when a user creates a quiz (CREATE QUIZ)
 const addNewQuiz = function() {
 }
-
-getSelectedQuiz();
 
 // module.exports = { getQuizzes };
