001a22ce999327b568ba2bac5b368371d8eee21a
Daniel <danielseniv@outlook.com>
2023-03-03 05:44:05 -0500

    Add query to insert 'create quiz' data to database

--- db/queries/quizzes.js
+++ db/queries/quizzes.js
@@ -15,7 +15,7 @@ const getSelectedQuiz = function(quiz_id) {
   return db.query('SELECT quizzes.id, title, description, quiz_questions.* FROM quiz_questions JOIN quizzes ON quiz_id = quizzes.id')
   .then(data => {
     const quizData = data.rows;
-    return quizData; //will make this function parameterized
+    return quizData; //will make this function parameterized based on whatever quiz_id is, just wanted to make sure it works
   })
 }
 
@@ -24,11 +24,14 @@ const getSelectedQuiz = function(quiz_id) {
 const addQuizResult = function(quizResultData) {
   const queryParams = [quizResultData.id, quizResultData.quiz_id, quizResultData.user_id, quizResultData.score, quizResultData.completed_at, quizResultData.link];
   const parameterizedQuery = 'INSERT INTO quiz_results VALUES ($1, $2, $3, $4, $5, $6)';
-  return db.query(parameterizedQuery, queryParams);
+  return db.query(parameterizedQuery, queryParams); //tested with fake object
 }
 
 // insert new quiz to relevant tables when a user creates a quiz (CREATE QUIZ)
-const addNewQuiz = function() {
+const addNewQuiz = function(newQuizData) {
+  const queryParams = [newQuizData.id, newQuizData.owner_id, newQuizData.title, newQuizData.description, newQuizData.link, newQuizData.is_public, newQuizData.is_active, newQuizData.total_attempts, newQuizData.thumbnail_photo_url, newQuizData.cover_photo_url];
+  const parameterizedQuery = 'INSERT INTO quizzes VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)';
+  return db.query(parameterizedQuery, queryParams); //tested with fake object
 }
 
 // module.exports = { getQuizzes };
