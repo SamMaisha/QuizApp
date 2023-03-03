a8fd1366ab1770d81c399fb84ab8083bdede02fb
Daniel <danielseniv@outlook.com>
2023-03-03 05:13:46 -0500

    Add query to return all quizzes object

--- db/queries/quizzes.js
+++ db/queries/quizzes.js
@@ -2,14 +2,19 @@ const db = require('../connection');
 
 // get all public quizzes from quizzes table (HOMEPAGE)
 const getQuizzes = () => {
-  return db.query('SELECT * FROM users;')
+  return db.query('SELECT * FROM quizzes;')
     .then(data => {
-      return data.rows;
+      const allQuizzes = data.rows;
+      return allQuizzes;
     });
 };
 
 // get individual quiz from quizzes table when user clicks on a quiz - show quiz questions and options (VIEW QUIZ)
-const getSelectedQuiz = function() {
+const getSelectedQuiz = function(quiz_id) {
+  return db.query('SELECT quizzes.id, title, description, quiz_questions.* FROM quiz_questions JOIN quizzes ON quiz_id = quizzes.id')
+  .then(data => {
+    console.log(data.rows);
+  })
 }
 
 // insert results to quiz_results table after user has taken quiz (VIEW QUIZ)
@@ -20,6 +25,6 @@ const addQuizResult = function() {
 const addNewQuiz = function() {
 }
 
+getSelectedQuiz();
 
-
-module.exports = { getQuizzes };
+// module.exports = { getQuizzes };
