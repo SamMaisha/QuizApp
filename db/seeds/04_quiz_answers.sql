71466f51a4e21acca00aa40d211c3cb1692fff8e
Daniel <danielseniv@outlook.com>
2023-03-04 14:57:06 -0500

    Replace initial lorum ipsum data with demo data

--- db/seeds/04_quiz_answers.sql
+++ db/seeds/04_quiz_answers.sql
@@ -1,5 +1,136 @@
-insert into quiz_answers (id, quiz_id, question_id, is_correct) values (1, 1, 5, true);
-insert into quiz_answers (id, quiz_id, question_id, is_correct) values (2, 1, 5, false);
+--First set of answers
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (1, 1, 1, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (2, 1, 1, false);
 insert into quiz_answers (id, quiz_id, question_id, is_correct) values (3, 1, 1, false);
-insert into quiz_answers (id, quiz_id, question_id, is_correct) values (4, 1, 2, true);
-insert into quiz_answers (id, quiz_id, question_id, is_correct) values (5, 1, 2, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (4, 1, 1, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (5, 1, 2, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (6, 1, 2, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (7, 1, 2, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (8, 1, 2, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (9, 1, 3, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (10, 1, 3, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (11, 1, 3, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (12, 1, 3, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (13, 1, 4, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (14, 1, 4, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (15, 1, 4, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (16, 1, 4, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (17, 1, 5, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (18, 1, 5, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (19, 1, 5, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (20, 1, 5, false);
+
+--Second set of answers
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (21, 2, 1, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (22, 2, 1, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (23, 2, 1, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (24, 2, 1, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (25, 2, 2, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (26, 2, 2, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (27, 2, 2, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (28, 2, 2, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (29, 2, 3, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (30, 2, 3, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (31, 2, 3, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (32, 2, 3, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (33, 2, 4, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (34, 2, 4, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (35, 2, 4, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (36, 2, 4, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (37, 2, 5, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (38, 2, 5, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (39, 2, 5, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (40, 2, 5, false);
+
+--Third set of answers
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (41, 3, 1, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (42, 3, 1, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (43, 3, 1, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (44, 3, 1, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (45, 3, 2, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (46, 3, 2, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (47, 3, 2, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (48, 3, 2, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (49, 3, 3, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (50, 3, 3, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (51, 3, 3, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (52, 3, 3, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (53, 3, 4, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (54, 3, 4, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (55, 3, 4, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (56, 3, 4, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (57, 3, 5, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (58, 3, 5, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (59, 3, 5, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (60, 3, 5, false);
+
+--Fourth set of answers
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (61, 4, 1, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (62, 4, 1, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (63, 4, 1, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (64, 4, 1, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (65, 4, 2, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (66, 4, 2, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (67, 4, 2, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (68, 4, 2, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (69, 4, 3, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (70, 4, 3, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (71, 4, 3, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (72, 4, 3, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (73, 4, 4, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (74, 4, 4, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (75, 4, 4, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (76, 4, 4, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (77, 4, 5, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (78, 4, 5, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (79, 4, 5, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (80, 4, 5, false);
+
+--Fifth set of answers
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (81, 5, 1, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (82, 5, 1, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (83, 5, 1, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (84, 5, 1, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (86, 5, 2, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (87, 5, 2, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (88, 5, 2, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (89, 5, 2, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (90, 5, 3, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (91, 5, 3, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (92, 5, 3, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (93, 5, 3, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (94, 5, 4, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (95, 5, 4, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (96, 5, 4, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (97, 5, 4, false);
+
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (98, 5, 5, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (99, 5, 5, true);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (100, 5, 5, false);
+insert into quiz_answers (id, quiz_id, question_id, is_correct) values (101, 5, 5, false);
+
+
