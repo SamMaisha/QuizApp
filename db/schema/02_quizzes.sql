8ae2718b6a584886e069b642fe5a497ac7861bf1
Daniel <danielseniv@outlook.com>
2023-03-05 16:38:23 -0500

    Change schema and seed data to incorporate type column

--- db/schema/02_quizzes.sql
+++ db/schema/02_quizzes.sql
@@ -6,6 +6,7 @@ CREATE TABLE quizzes (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) NOT NULL,
   title VARCHAR(255) NOT NULL,
+  type VARCHAR(255) NOT NULL,
   description TEXT,
   link VARCHAR(255) NOT NULL,
   is_public BOOLEAN DEFAULT true,
