246bcee34147b6f3e231db61e29ed45e4b55231e
Daniel <danielseniv@outlook.com>
2023-03-05 16:37:55 -0500

    Change to param queries, tailor queries to return more relevant data

--- db/queries/users.js
+++ db/queries/users.js
@@ -4,14 +4,23 @@ const db = require('../connection');
 // get quiz results for user from quiz_results table (QUIZ RESULT)
 getQuizResultsForUser = function(user) {
     const queryParams = [user.id];
-    const parameterizedQuery = 'SELECT * FROM quiz_results WHERE user_id = $1'
-    return db.query(parameterizedQuery,queryParams);
-}
+    const parameterizedQuery = 'SELECT quiz_results.*, quizzes.title FROM quiz_results JOIN quizzes ON quiz_id = quizzes.id WHERE user_id = $1'
+    return db.query(parameterizedQuery,queryParams)
+      .then(data => {
+        const quizResults = data.rows;
+        console.log(quizResults);
+        return quizResults;
+      });
+};
 
 // get quizzes that were created by a user (MY QUIZZES)
 getQuizzesCreatedByUser = function(user) {
     const queryParams = [user.id];
     const parameterizedQuery = 'SELECT * FROM quizzes WHERE owner_id = $1'
-    return db.query(parameterizedQuery,queryParams);
+    return db.query(parameterizedQuery,queryParams)
+      .then(data => {
+        const userCreatedQuiz = data.rows;
+        console.log(userCreatedQuiz);
+        return userCreatedQuiz;
+      });
 }
-
