16440bf5e10e83de47758b17460b57c41d7bebb6
Daniel <danielseniv@outlook.com>
2023-03-06 19:36:26 -0500

    Change back to old name column, instead alias the name in query

--- db/schema/03_quiz_questions.sql
+++ db/schema/03_quiz_questions.sql
@@ -3,7 +3,7 @@
 DROP TABLE IF EXISTS quiz_questions CASCADE;
 
 CREATE TABLE quiz_questions (
-  question_id SERIAL PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   quiz_id INTEGER REFERENCES quizzes(id),
   question TEXT NOT NULL,
   photo_url VARCHAR(255)
--- db/seeds/03_quiz_questions.sql
+++ db/seeds/03_quiz_questions.sql
@@ -1,32 +1,32 @@
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (1, 1, 'What is the most abundant mineral in the human body?', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (2, 1, 'The smallest bones in the body are found in the...?', 'http://dummyimage.com/135x100.png/dddddd/000000');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (3, 1, 'Eye colour is defined by which circular structure?', 'http://dummyimage.com/103x100.png/cc0000/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (4, 1, 'What is hardest and most highly mineralized substance of the body?', 'http://dummyimage.com/122x100.png/cc0000/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (5, 1, 'What is the largest muscle in the human body?', 'http://dummyimage.com/148x100.png/dddddd/000000');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (1, 1, 'What is the most abundant mineral in the human body?', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (2, 1, 'The smallest bones in the body are found in the...?', 'http://dummyimage.com/135x100.png/dddddd/000000');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (3, 1, 'Eye colour is defined by which circular structure?', 'http://dummyimage.com/103x100.png/cc0000/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (4, 1, 'What is hardest and most highly mineralized substance of the body?', 'http://dummyimage.com/122x100.png/cc0000/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (5, 1, 'What is the largest muscle in the human body?', 'http://dummyimage.com/148x100.png/dddddd/000000');
 
 
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (6, 2, 'Which of the following is NOT something the ghost tells Hamlet?', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (7, 2, 'The performance of ''The Murder of Gonzaga'' is an example of which literary device?', 'http://dummyimage.com/135x100.png/dddddd/000000');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (8, 2, 'Why doesn''t Hamlet kill Claudius in the church?', 'http://dummyimage.com/103x100.png/cc0000/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (9, 2, 'Which character is accidentally killed by Hamlet', 'http://dummyimage.com/122x100.png/cc0000/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (10, 2, 'Why does Laertes return to Denmark?', 'http://dummyimage.com/148x100.png/dddddd/000000');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (6, 2, 'Which of the following is NOT something the ghost tells Hamlet?', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (7, 2, 'The performance of ''The Murder of Gonzaga'' is an example of which literary device?', 'http://dummyimage.com/135x100.png/dddddd/000000');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (8, 2, 'Why doesn''t Hamlet kill Claudius in the church?', 'http://dummyimage.com/103x100.png/cc0000/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (9, 2, 'Which character is accidentally killed by Hamlet', 'http://dummyimage.com/122x100.png/cc0000/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (10, 2, 'Why does Laertes return to Denmark?', 'http://dummyimage.com/148x100.png/dddddd/000000');
 
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (11, 3, 'What does an ohm measure?', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (12, 3, 'Which of these is NOT a state of matter?', 'http://dummyimage.com/135x100.png/dddddd/000000');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (13, 3, 'True or false: Velocity and acceleration have the same meaning.', 'http://dummyimage.com/103x100.png/cc0000/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (14, 3, 'The ability of a substance to return to its shape after being deformed is called ______.', 'http://dummyimage.com/122x100.png/cc0000/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (15, 3, 'In nuclear physics, what is the opposite of fusion?', 'http://dummyimage.com/148x100.png/dddddd/000000');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (11, 3, 'What does an ohm measure?', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (12, 3, 'Which of these is NOT a state of matter?', 'http://dummyimage.com/135x100.png/dddddd/000000');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (13, 3, 'True or false: Velocity and acceleration have the same meaning.', 'http://dummyimage.com/103x100.png/cc0000/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (14, 3, 'The ability of a substance to return to its shape after being deformed is called ______.', 'http://dummyimage.com/122x100.png/cc0000/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (15, 3, 'In nuclear physics, what is the opposite of fusion?', 'http://dummyimage.com/148x100.png/dddddd/000000');
 
 
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (16, 4, 'In angiosperms, the endosperm functions in', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (17, 4, 'Which of the following features of angiosperms has probably contributed most to their evolutionary success relative to all other land plant groups?', 'http://dummyimage.com/135x100.png/dddddd/000000');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (18, 4, 'Only female gametes are produced in which of the following structures?', 'http://dummyimage.com/103x100.png/cc0000/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (19, 4, 'Which of the following statements about xylem and phloem is correct?', 'http://dummyimage.com/122x100.png/cc0000/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (20, 4, 'Which of the following factors is most important in the movement of water up a tall tree?', 'http://dummyimage.com/148x100.png/dddddd/000000');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (16, 4, 'In angiosperms, the endosperm functions in', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (17, 4, 'Which of the following features of angiosperms has probably contributed most to their evolutionary success relative to all other land plant groups?', 'http://dummyimage.com/135x100.png/dddddd/000000');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (18, 4, 'Only female gametes are produced in which of the following structures?', 'http://dummyimage.com/103x100.png/cc0000/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (19, 4, 'Which of the following statements about xylem and phloem is correct?', 'http://dummyimage.com/122x100.png/cc0000/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (20, 4, 'Which of the following factors is most important in the movement of water up a tall tree?', 'http://dummyimage.com/148x100.png/dddddd/000000');
 
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (21, 5, 'What does Berger (1963) describe as a metaphor for social reality?', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (22, 5, 'What is the name of the process by which we acquire a sense of identity and become members of society?', 'http://dummyimage.com/135x100.png/dddddd/000000');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (23, 5, 'In contemporary societies, social institutions are:', 'http://dummyimage.com/103x100.png/cc0000/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (24, 5, 'Which of the following is not recognised as a level of society?', 'http://dummyimage.com/122x100.png/cc0000/ffffff');
-insert into quiz_questions (question_id, quiz_id, question, photo_url) values (25, 5, 'When sociologists study the structure of layers in society and people''s movement between them, they call this:', 'http://dummyimage.com/148x100.png/dddddd/000000');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (21, 5, 'What does Berger (1963) describe as a metaphor for social reality?', 'http://dummyimage.com/154x100.png/ff4444/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (22, 5, 'What is the name of the process by which we acquire a sense of identity and become members of society?', 'http://dummyimage.com/135x100.png/dddddd/000000');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (23, 5, 'In contemporary societies, social institutions are:', 'http://dummyimage.com/103x100.png/cc0000/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (24, 5, 'Which of the following is not recognised as a level of society?', 'http://dummyimage.com/122x100.png/cc0000/ffffff');
+insert into quiz_questions (id, quiz_id, question, photo_url) values (25, 5, 'When sociologists study the structure of layers in society and people''s movement between them, they call this:', 'http://dummyimage.com/148x100.png/dddddd/000000');
 
