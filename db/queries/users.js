757638158922afa872ae685ca02d255a18a3e08b
Daniel <danielseniv@outlook.com>
2023-03-08 23:13:38 -0500

    Add getUserById, getUserByEmail, fix addUser by returning all

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,6 +1,32 @@
 const { param } = require('express/lib/request');
 const db = require('../connection');
 
+//get user credentials by ID
+const getUserById = function (userID) {
+  const queryParams = [userID];
+  const parameterizedQuery = `
+  SELECT users.email, users.password, users.id FROM users WHERE users.id = $1 
+  `
+  return db.query(parameterizedQuery, queryParams)
+    .then(data => {
+      const userCreds = data.rows[0];
+      return userCreds;
+    })
+};
+
+//get user credentials by email
+const getUserIdByEmail = function(email) {
+  const queryParams = [email];
+  const parameterizedQuery = `
+  SELECT users.id FROM users WHERE users.email = $1
+  `
+  return db.query(parameterizedQuery, queryParams)
+    .then(data => {
+      const userId = data.rows[0];
+      return userId;
+    })
+};
+
 // get quiz results for user from quiz_results table (QUIZ RESULT)
 const getQuizResultsForUser = function(userId) {
     const queryParams = [userId];
@@ -25,4 +51,22 @@ const getQuizzesCreatedByUser = function(userId) {
       });
 }
 
-module.exports = { getQuizResultsForUser, getQuizzesCreatedByUser };
+const addUser = function(userCredentials) { //tested with fake object, inserts
+  const queryParams = [userCredentials.name, userCredentials.email, userCredentials.password];
+  const parameterizedQuery = `
+  INSERT INTO users (name, email, password) VALUES ($1, $2, $3)
+  RETURNING *`
+  
+  console.log('worked');
+  return db.query(parameterizedQuery, queryParams);
+}
+
+const getAllEmails = function() {
+  return db.query(`SELECT email FROM users`)
+    .then(data => {
+      const allEmails = data.rows;
+      return allEmails;
+    })
+}
+
+module.exports = { getQuizResultsForUser, getQuizzesCreatedByUser, getUserById, getAllEmails, addUser, getUserIdByEmail };
